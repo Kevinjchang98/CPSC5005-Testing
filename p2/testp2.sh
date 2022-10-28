14d64640c74579aa1347db99418be800fe45f6ae
Kevin Chang <kevinjchang98@gmail.com>
2022-10-27 22:56:17 -0700

    format and color name output

--- p2/testp2.sh
+++ p2/testp2.sh
@@ -1,5 +1,8 @@
 #! /bin/bash
 
+BLUE=$(tput setaf 4)
+NORMAL=$(tput sgr0)
+
 printf "Project 2\n"
 
 printf "Making results folder\n"
@@ -12,12 +15,17 @@ printf "Going through submissions\n"
 for dir in $(find ./submissions -mindepth 1 -maxdepth 1 -type d)
 do
     NAME=${dir##*/}
-    printf "\n$NAME\n"
-    printf "\tBuilding test\n"
+    printf "\n$BLUE\n"
+    printf "===============================================================================\n"
+    printf "$NAME"
+    printf "\n==============================================================================="
+    printf "\n\tBuilding test\n"
 
     c++ test.cpp -std=c++2a -I../include/ -I$dir/ -o $dir/$NAME-test
 
     printf "\tRunning test\n"
+
+    printf "$NORMAL"
 
     ./$dir/$NAME-test > ./results/$NAME-result.txt
     ./$dir/$NAME-test 
